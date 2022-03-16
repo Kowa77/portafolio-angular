ee836a29a14541245b651968b40161029ad0faf3
Esteban Garriga <estebandavidgarriga@hotmail.com>
2022-03-16 18:36:06 -0300

    Productos ya cargados desde firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,22 +10,37 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any [] = [];
+
+
   constructor( private http: HttpClient ) { 
+    
+    this.cargarInfo();
+    this.cargarEquipo();
+  
+  }
 
-    console.log('Servicio de infoPagina Listo');
-
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {  //InfoPagina es la interface
         
-        this.cargada = true;
-        this.info = resp;     
-        
-        console.log( resp );
-        //console.log( resp['email'] );
-        //console.log( resp.twitter );
+      this.cargada = true;
+      this.info = resp;     
 
-        
-      });
+    });
   }
+
+  private cargarEquipo(){
+    
+    this.http.get('https://angular-html-8b5b3-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( ( resp: any ) => {  
+
+      this.equipo = resp;  
+      //console.log( this.equipo );
+     
+    });
+  }
+
+
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+
+  productos: Producto[] = [];
+ 
+
+  constructor( private http: HttpClient ) { 
+    
+    this.cargarProductos();
+  
+  }
+
+  private cargarProductos(){
+    this.http.get('https://angular-html-8b5b3-default-rtdb.firebaseio.com/productos_idx.json')
+      //InfoProductos esta comprobando los tipos desde la interface
+        .subscribe( (resp: any) => {  
+      
+          this.productos = resp;  
+          
+          this.cargando = false;
+
+          console.log(this.productos);
+
+    });
+  }
+
+
+}
